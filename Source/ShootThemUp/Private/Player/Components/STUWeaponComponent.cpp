3043778b698be9243d857d8211f19502ae36f0d7
Daniil Evstigneev <dany.evstigneev@gmail.com>
2022-09-10 16:56:57 +0300

    Gun trace

--- Source/ShootThemUp/Private/Player/Components/STUWeaponComponent.cpp
+++ Source/ShootThemUp/Private/Player/Components/STUWeaponComponent.cpp
@@ -37,5 +37,6 @@ void USTUWeaponComponent::SpawnWeapon()
 	
 	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, false);
 	CurrentWeapon->AttachToComponent(Character->GetMesh(), AttachmentRules, WeaponAttachPointName);
+	CurrentWeapon->SetOwner(GetOwner());
 	
 }
--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -21,8 +21,14 @@ public:
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
 	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	FName MuzzleSocketName = "MuzzleSocket";
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	float TraceMaxDistance = 1500.0f;
 	
 	virtual void BeginPlay() override;
 
-
+	void MakeShot();
 };
